bbb4b5ab99f5dad9114462cc669c005d825b1393
Aleksey Lipaev <alexeylipaev@yandex.ru>
2018-04-20 14:59:57 +0300

    ACSDinamikaWeb namespace changed to ACSWeb

--- ACSDinamikaWeb/ACSDinamikaWeb/Properties/AssemblyInfo.cs
+++ ACSDinamikaWeb/ACSDinamikaWeb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ACSDinamikaWeb")]
+[assembly: AssemblyTitle("ACSWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ACSDinamikaWeb")]
+[assembly: AssemblyProduct("ACSWeb")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
