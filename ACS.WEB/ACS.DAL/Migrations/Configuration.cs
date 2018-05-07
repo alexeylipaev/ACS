cb575067e771bd9af1ac6823845a85503b3653e9
Kachkov <Kachkov@TF09>
2018-05-07 14:34:30 +0300

    asd

--- ACS.WEB/ACS.DAL/Migrations/Configuration.cs
+++ ACS.WEB/ACS.DAL/Migrations/Configuration.cs
@@ -17,24 +17,24 @@ namespace ACS.DAL.Migrations
 
         protected override void Seed(ACSContext db)
         {
-            //Debug.WriteLine("GenerateAppRolesRepository");
-            //DataLoader1C.GenerateAppRolesRepository(db);
-            //Debug.WriteLine("GenerateApplicationUserSystem");
-            //DataLoader1C.GenerateApplicationUserSystem(db);
-            //Debug.WriteLine("GenerateUserRepository");
-            //DataLoader1C.GenerateUserRepository(db);
-            //Debug.WriteLine("GenerateDepartmentRepository");
-            //DataLoader1C.GenerateDepartmentRepository(db);
-            //Debug.WriteLine("GeneratePostRepository");
-            //DataLoader1C.GeneratePostRepository(db);
-            //Debug.WriteLine("GeneratePostsEmployees—ode1—Repository");
-            //DataLoader1C.GeneratePostsEmployees—ode1—Repository(db);
-            //Debug.WriteLine("GenerateWorkHistoryRepository");
-            //DataLoader1C.GenerateWorkHistoryRepository(db);
-            //Debug.WriteLine("GenerateTypeAccessRepository");
-            //DataLoader1C.GenerateTypeAccessRepository(db);
-            //Debug.WriteLine("GenerateTypeRecordChancelleryRepository");
-            //DataLoader1C.GenerateTypeRecordChancelleryRepository(db);
+            Debug.WriteLine("GenerateAppRolesRepository");
+            DataLoader1C.GenerateAppRolesRepository(db);
+            Debug.WriteLine("GenerateApplicationUserSystem");
+            DataLoader1C.GenerateApplicationUserSystem(db);
+            Debug.WriteLine("GenerateUserRepository");
+            DataLoader1C.GenerateUserRepository(db);
+            Debug.WriteLine("GenerateDepartmentRepository");
+            DataLoader1C.GenerateDepartmentRepository(db);
+            Debug.WriteLine("GeneratePostRepository");
+            DataLoader1C.GeneratePostRepository(db);
+            Debug.WriteLine("GeneratePostsEmployees—ode1—Repository");
+            DataLoader1C.GeneratePostsEmployees—ode1—Repository(db);
+            Debug.WriteLine("GenerateWorkHistoryRepository");
+            DataLoader1C.GenerateWorkHistoryRepository(db);
+            Debug.WriteLine("GenerateTypeAccessRepository");
+            DataLoader1C.GenerateTypeAccessRepository(db);
+            Debug.WriteLine("GenerateTypeRecordChancelleryRepository");
+            DataLoader1C.GenerateTypeRecordChancelleryRepository(db);
         }
 
 
